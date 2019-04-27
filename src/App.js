0bce2b2668d0a6305345720abe35f16663fefff3
abhidevlopment <abhidevlopment4@gmail.com>
2019-04-27 18:10:31 +0530

    commited by abhidevlopment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Error from "./components/Error";
+import About from "./components/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navigation />
+
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <NavLink to="/">Home</NavLink>|<NavLink to="/about">About</NavLink>|
+        <NavLink to="/contact">Contact</NavLink>
+        <hr />
+      </div>
+    );
+  }
+}
+export default Navigation;
